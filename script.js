9ea54d42d5d544add183716361b514e9b92fa6e5
Payal Chaudhary <payalchaudhary9627@gmail.com>
2026-03-27 13:16:22 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function changeText() {
+  document.getElementById("text").innerText = "You clicked the button!";
+}
+
+function changeColor() {
+  document.body.style.backgroundColor = "lightblue";
+}
+
+function hideText() {
+  document.getElementById("text").style.display = "none";
+}
